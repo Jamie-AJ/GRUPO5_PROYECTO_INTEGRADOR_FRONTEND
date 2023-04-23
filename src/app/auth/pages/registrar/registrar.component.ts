7baec8b0bb61f1385150a3c85aac620466ca3aba
RaulQD <raul.g.quispe@gmail.com>
2023-04-23 01:15:03 -0500

    limpiando codigo

--- src/app/auth/pages/registrar/registrar.component.ts
+++ src/app/auth/pages/registrar/registrar.component.ts
@@ -36,8 +36,5 @@ export class RegistrarComponent implements OnInit {
     this.router.navigate(['/dashboard/home']);
     console.log('crear usuario');
   }
-  createRol(){
-    console.log('crear rol asdasd');
-    console.log('this.roles',this.roles);
-  }
+
 }
