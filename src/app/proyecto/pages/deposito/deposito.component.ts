27d5d1be3c7464a8bbb99013ebf89a9e2cb123be
Jeimy Apolaya <jfaj-04@hotmail.com>
2023-05-25 12:01:14 -0500

    Deposito y deuda terminado

--- src/app/proyecto/pages/deposito/deposito.component.ts
+++ src/app/proyecto/pages/deposito/deposito.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { CuentaBancaria } from 'src/app/interface/cuentaBancaria.interface';
+import { Transaccion } from 'src/app/interface/transaccion.interface';
+import { CuentaBancariaService } from 'src/app/services/cuenta-bancaria.service';
+import { TransaccionService } from 'src/app/services/transaccion.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-deposito',
@@ -7,8 +12,51 @@ import { Router } from '@angular/router';
   styleUrls: ['./deposito.component.css']
 })
 export class DepositoComponent {
+  objTransaccion: Transaccion = new Transaccion();
+  /*objTransaccion:Transaccion = {
+    idCuentaBancaria:0,
+    monto: 0
+  };*/
+  cuentabancariaCombo: CuentaBancaria[] = [];
+
+  constructor(private cuentaBancariaService: CuentaBancariaService, private transaccionService: TransaccionService){
+   }
+  
+  ngOnInit() {
+    this.obtenerCuentasBancarias();
+  }
+
+
   @Output() depositoCompletado = new EventEmitter<void>();
   private router = inject(Router);
+
+  obtenerCuentasBancarias(){
+    this.cuentaBancariaService.getCuentaBancaria().subscribe(
+      cuentas => {
+        this.cuentabancariaCombo = cuentas;
+        console.log(cuentas);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  postDepositar(){
+    this.transaccionService.postDeposito(this.objTransaccion).subscribe(
+      response =>{
+        Swal.fire('Depositado con Exito', response.mensaje, 'success');
+        this.router.navigate(['/dashboard/account-status']);
+      },
+      error =>{
+        console.error(error);
+        if (error.status === 400) {
+          console.error(error.error.mensaje);
+        } else {
+        }
+      }
+    );
+  }
 
   finalizarDeposito() {
     // Restablecer el estado del componente principal
--- src/app/services/transaccion.service.ts
+++ src/app/services/transaccion.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, pipe, throwError } from 'rxjs';
 import { TipoTransaccion } from '../interface/tipoTransaccion.interface';
 import { Transaccion } from '../interface/transaccion.interface';
+import Swal from 'sweetalert2';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,28 @@ import { Transaccion } from '../interface/transaccion.interface';
 export class TransaccionService {
 
   private http = inject(HttpClient);
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
   url: string = 'http://localhost:8091/api';
   // getTipoTransaction():Observable<TipoTransaccion>{
   // }
 
   getTransaction():Observable<Transaccion[]>{
     return this.http.get<Transaccion[]>(`${this.url}/user/listaTransacciones`);
+  }
+  postDeposito(transaccion:Transaccion):Observable<any>{
+    return this.http.post<any>(`${this.url}/deposito`,transaccion,{headers:this.httpHeaders}).pipe(
+      catchError(error =>{
+        Swal.fire('Error', error.error.mensaje, 'error');
+        return throwError(error);
+      })
+    );
+  }
+  postRetiro(transaccion:Transaccion):Observable<any>{
+    return this.http.post<any>(`${this.url}/retiro`,transaccion,{headers:this.httpHeaders}).pipe(
+      catchError(error =>{
+        Swal.fire('Error', error.error.mensaje, 'error');
+        return throwError(error);
+      })
+    );
   }
 }
