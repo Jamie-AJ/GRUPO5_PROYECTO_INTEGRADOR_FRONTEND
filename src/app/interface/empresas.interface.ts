cf273fd396e1c813edcb1bd61256513786b6fd52
RaulQD <raul.g.quispe@gmail.com>
2023-06-22 17:32:39 -0500

    arreglos minimos

--- src/app/interface/empresas.interface.ts
+++ src/app/interface/empresas.interface.ts
@@ -1,14 +1,17 @@
+import { Riesgo } from "./riesgo.interface";
+
 export class Empresas {
-    idEmpresa?:         number;
-    nomEmpresa?:        string;
-    ruc?:               string;
-    razonSocial?:       string;
-    representanteLegal?:  string;
+    idEmpresa?: number;
+    nomEmpresa?: string;
+    ruc?: string;
+    razonSocial?: string;
+    representanteLegal?: string;
     fechaDeInicioActv?: Date;
-    direccion?:         string;
-    telefono?:          string;
-    correo?:            string;
-    sector?:            string;
-    fechRegistro?:      Date;
-    enable?:            string;
+    direccion?: string;
+    telefono?: string;
+    correo?: string;
+    sector?: string;
+    fechRegistro?: Date;
+    enable?: string;
+    riesgo?: Riesgo;
 }
---
+++ src/app/interface/riesgo.interface.ts
@@ -0,0 +1,5 @@
+export class Riesgo{
+    idRiesgo?: number;
+    descripcion?: string;
+    rango?: string;
+}
